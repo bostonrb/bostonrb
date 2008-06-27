a17368e3a122bca76ee69b3c389b238c4c2aa43c
Josh Nichols <josh@technicalpickles.com>
2008-06-26 21:46:39 -0400

    Events by default.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.site 'site/:name', :controller => 'pages', :action => 'show'
 
-  map.home '/', :controller => 'projects', :action => 'index'
+  map.home '/', :controller => 'events', :action => 'index'
 
   # Install the default routes as the lowest priority.
   # map.connect ':controller/:action/:id'
