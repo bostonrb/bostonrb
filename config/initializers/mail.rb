93af99f075cf164cbe50da4a690a0d49e64e3780
Dan Croak <dcroak@thoughtbot.com>
2008-09-23 20:34:12 -0400

    removing exception_notification and akismetor plugins

--- config/initializers/mail.rb
+++
@@ -1,3 +0,0 @@
-ExceptionNotifier.sender_address = "'Boston.rb Error' <dcroak@thoughtbot.com>"
-ExceptionNotifier.email_prefix = "[Boston.rb] "
-ExceptionNotifier.exception_recipients = %w(dcroak@thoughtbot.com)
