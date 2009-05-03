a4908c6e68037814835b21f829225b1d03458276
Dan Croak <dcroak@thoughtbot.com>
2009-05-02 23:50:32 -0400

    home page design good for now

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,10 +1,16 @@
 module JobsHelper
-  
+
   def list_job job
     html = "<strong>#{job.location}</strong>: "
     html << link_to(job.title, job_url(job))
     html << " at #{job.organization}"
     html
   end
-  
+
+  def job_title_and_company_name
+    job     = html_escape("Rails developer")
+    company = html_escape("thoughtbot")
+    "#{link_to(job, '#')} &ndash; #{company}"
+  end
+
 end
