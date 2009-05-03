1695d6b3c651a2fb7f11b7bfdcb4873ae3ca4cef
Dan Croak <dcroak@thoughtbot.com>
2009-05-03 14:31:22 -0400

    removing helper test for removed helper method

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1,18 +1,4 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  context 'ApplicationHelper#blueprint_ie' do
-    setup do
-      @result = blueprint_ie
-    end
-
-    should 'not be nil' do
-      assert_not_nil @result
-    end
-
-    should 'do magic ie comment to only load in IE' do
-      expected = %q(<!--[if IE]>)
-      assert @result.include?(expected)
-    end
-  end
 end
