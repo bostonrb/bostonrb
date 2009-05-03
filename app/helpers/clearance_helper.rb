ac29cf3cff939babbae2d52079d3fcad3992f90c
Dan Croak <dcroak@thoughtbot.com>
2009-05-03 15:54:52 -0400

    named routes for authentication. authentication links helper.

--- app/helpers/clearance_helper.rb
+++ app/helpers/clearance_helper.rb
@@ -8,12 +8,11 @@ module ClearanceHelper
   end
 
   def signed_out_links
-    links = { "Sign up" => new_user_path, "Sign in" => new_session_path }
+    links = { "Sign up" => sign_up_path, "Sign in" => sign_in_path }
     links.collect { |key, value| link_to(key, value) }
   end
 
   def signed_in_links
-    links = { "Sign up" => new_user_path, "Sign in" => new_session_path }
-    links.collect { |key, value| link_to(key, value) }
+    [link_to("Sign out", sign_out_path)]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,16 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :places
 
   map.root :controller => 'home', :action => 'index'
+
+  map.sign_up  'sign_up',
+    :controller => 'clearance/users',
+    :action     => 'new'
+  map.sign_in  'sign_in',
+    :controller => 'clearance/sessions',
+    :action     => 'new'
+  map.sign_out 'sign_out',
+    :controller => 'clearance/sessions',
+    :action     => 'destroy',
+    :method     => :delete
 end
 
