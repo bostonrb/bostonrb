cb9d3445c10c431c2ebb6550b57f1f412980590f
Dan Croak <dcroak@thoughtbot.com>
2009-05-03 14:29:53 -0400

    put 'about' information in footer. converted 'about' page to 'sponsors' page. removed unused helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
-  def blueprint_ie
-    html = "<!--[if IE]>"
-    html << stylesheet_link_tag('blueprint/ie', :media => 'screen')
-    html << "<![endif]-->"
-    html
-  end
-  
+
   def tab_link_attributes(name)
     attributes = {}
     attributes[:class] = 'current' if controller.controller_name == name
     attributes
   end
- 
-  def tab_for(name, url = nil)
-    url ||= send("#{name.downcase}_url")
-    "<li>#{link_to name.capitalize, url, tab_link_attributes(name)}</li>"
-  end
-  
+
   def anticaptcha
     content_tag(:div, :class => 'captcha') do
       label_tag(:captcha, 'Answer this if you are a roboto') + text_field_tag(:captcha)
--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class PagesControllerTest < ActionController::TestCase
 
-  %w(about).each do |page|
+  %w(sponsors).each do |page|
     context "on GET to /pages/#{page}" do
       setup { get :show, :id => page }
 
