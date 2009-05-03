ec097a276919378608d97c3250f83d5eb49f3b4d
Dan Croak <dcroak@thoughtbot.com>
2009-05-03 01:19:34 -0400

    fixed a few inconsistencies in the UI

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,10 @@
 module ProjectsHelper
 
-  def project_name_and_link
-    name = html_escape("Jeweler")
-    link = html_escape("http://github.com/techpickles/jeweler")
-    "#{name} &ndash; #{link_to(link, link)}"
+  def project_name_and_description
+    name        = html_escape("Jeweler")
+    link        = html_escape("http://github.com/techpickles/jeweler")
+    description = html_escape("Simple and opinionated helper for creating and managing Rubygem projects on GitHub")
+    "#{link_to(name, link)} &ndash; #{description}"
   end
 
 end
