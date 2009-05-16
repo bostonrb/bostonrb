c0e30e2aae13ab5f5e604f21f1ae940caf482994
Josh Nichols <josh@technicalpickles.com>
2009-05-16 17:20:22 -0400

    Use javascript to show/hide 'crud' actions * switched to jquery, removed stale javascript files * use jquery to show/hide crud actions on hover

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,23 +1,10 @@
-function show_crud(event) {
-  element = event.element();
-  element.down(".crud").show();
-}
-
-function hide_crud(event) {
-  element = event.element();
-  if(!this.descendantOf(element)) {
-    element.down(".crud").hide();
-  }
-}
-
-function hide_all_crud() {
-  $("events").down(".crud").hide();
-}
-
-function bindUI() {
-  hide_all_crud();
-  $("events").observe("mouseover", show_crud);
-  $("events").observe("mouseout", hide_crud);
-}
-
-document.observe('dom:loaded', bindUI);
+jQuery(document).ready(function($) {
+    $('#events').hover(
+      function() {
+        $(this).find('.crud').show('blind');
+      },
+      function() {
+        $(this).find('.crud').hide('blind');
+      }
+    );
+});
