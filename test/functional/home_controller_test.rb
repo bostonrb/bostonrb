539a3d3cb05c8acdd0fcd6ae9a915f974941646e
Josh Nichols <josh@technicalpickles.com>
2009-05-17 23:53:43 -0400

    Moved user lookup into controller instead of view.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -2,10 +2,21 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   context 'GET to index' do
-    setup { get :index }
+    setup do
+      @user = Factory.build(:user)
+      @users = [@user]
+      User.stubs(:all).returns(@users)
+      get :index
+    end
 
     should_respond_with :success
     should_render_template :index
+
+    should "fetch users" do
+      assert_received(User, :all)
+    end
+
+    should_assign_to(:users) { @users }
   end
 
   context "given a future recurring event on GET to index" do
