5e33b05a7880ffbbd87446960d612152f56a37b2
Dan Croak <dcroak@thoughtbot.com>
2009-05-19 00:14:52 -0400

    pushing logic into the Tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :twitter_id, :text, :tweeted_at, :user_id
+
+  def self.recent(number)
+    ordered("tweeted_at desc").limited(number)
+  end
 end
--- test/unit/tweet_test.rb
+++ test/unit/tweet_test.rb
@@ -7,4 +7,20 @@ class TweetTest < ActiveSupport::TestCase
   should_belong_to  :user
   should_have_index :user_id
   should_validate_presence_of :text, :twitter_id, :tweeted_at, :user_id
+
+  context "recent five Tweets given six existing Tweets" do
+    setup do
+      6.times { |i| Factory(:tweet, :tweeted_at => i.minutes.ago) }
+      @tweets = Tweet.recent(5)
+    end
+
+    should "only find five tweets" do
+      assert_equal 5, @tweets.size
+    end
+
+    should "order tweets by tweeted at" do
+      expected = @tweets.sort_by(&:tweeted_at).reverse
+      assert_equal expected, @tweets
+    end
+  end
 end
