4d8c220de53b5ddb44bb4839504dd3b8c61bdcae
Dan Pickett <dpickett@enlightsolutions.com>
2009-05-20 09:48:34 +0800

    url validation plugin and macro
    Signed-off-by: Boston Ruby Group <bostonrb@technicalpickles.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 
+require 'macros/should_validate_url_format_of'
+
 class ActiveSupport::TestCase
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
