fa90b47636d32796fe9bece736c70feb608b5860
Dan Croak <dcroak@thoughtbot.com>
2009-05-21 00:10:28 -0400

    added test for collage_of(users)

--- test/unit/helpers/users_helper_test.rb
+++ test/unit/helpers/users_helper_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+  context "collage of users" do
+    setup do
+      @users   = [Factory.stub(:user, :gravatar_url => "http://p.ng")]
+      @collage = collage_of(@users)
+    end
+
+    should "be images based on gravatar URLs of users" do
+      expected = [image_tag("http://p.ng")]
+      assert_equal expected, @collage
+    end
+  end
 end
 
