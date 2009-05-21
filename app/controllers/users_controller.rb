620b00217fd18282eb302ab32d80187ac7a8c700
Dan Croak <dcroak@thoughtbot.com>
2009-05-21 02:35:38 -0400

    forbid editing user other than current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < Clearance::UsersController
+
+  before_filter :forbid_editing_user_other_than_current_user,
+    :only => [:edit, :update]
   skip_before_filter :authenticate, :only => [:new, :create]
+
 
   def edit
     @user = User.find(params[:id])
@@ -14,5 +18,12 @@ class UsersController < Clearance::UsersController
       render :action => :edit
     end
   end
+
+  def forbid_editing_user_other_than_current_user
+    unless User.find(params[:id]) == current_user
+      raise ActionController::Forbidden, "not current user"
+    end
+  end
+
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,3 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
   def tab_link_attributes(name)
