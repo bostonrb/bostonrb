0880c3e6b5f8536482683c41d4992ed8b00270d0
Dan Croak <dcroak@thoughtbot.com>
2009-05-21 18:59:49 -0400

    fixing failing presentations test

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,3 +1,5 @@
 class PresentationsController < ResourceController::Base
-  create.wants.html { redirect_to root_url }
+  create.wants.html  { redirect_to root_url }
+  destroy.wants.html { redirect_to root_url }
+  destroy.flash "Presentation deleted."
 end
