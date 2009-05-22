c815bf8903ddaeec085207cfef436f65c2e1d82d
Harold A. Giménez <harold.gimenez@gmail.com>
2009-05-22 16:08:45 -0400

    Added tests for main_url and has_link? on User model. Implemented twitter cleanout in the backend.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,18 @@ class User < ActiveRecord::Base
   attr_accessible :twitter
   attr_accessible :feed_attributes
 
+  validates_format_of :twitter, :with => /^[a-zA-Z][a-z0-9A-Z]{1,15}$/, :allow_nil => true
+  before_validation :cleanout_twitter
+
+
   def to_s
     twitter
   end
 
-  #For now, the twitter url.
-  #If users will have blogs, we could use that instead,
-  #and fall back to twitter otherwise.
+  # For now, the twitter url.
+  # Eventually we'll just use user_path for gravatar links
+  # and this method may just become main_external_url,
+  # which may be their blog or their twitter.
   def main_url
     return nil unless twitter
     "http://twitter.com/#{twitter.downcase}"
@@ -28,5 +33,11 @@ class User < ActiveRecord::Base
   def has_link?
     !!main_url
   end
+
+  private
+
+    def cleanout_twitter
+      twitter.gsub!(/^@/, '') if twitter
+    end
 
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -16,6 +16,12 @@ class UserTest < ActiveSupport::TestCase
     should_allow_mass_assignment_of :twitter
     should_allow_mass_assignment_of :feed_attributes
 
+    should_allow_values_for :twitter, 'zomg'
+    should_allow_values_for :twitter, '@aieeeee' #before_validation cleans this up
+    should_not_allow_values_for :twitter, 'http://twitter.com/wtf'
+    should_not_allow_values_for :twitter, 'foo@fail'
+
+
     should "display twitter name as string representation" do
       assert_equal "Croaky", @user.to_s
     end
@@ -25,4 +31,68 @@ class UserTest < ActiveSupport::TestCase
     end
 
   end
+
+  context "creating a user with a twitter" do
+    context "when the twitter name is has a leading @ sign" do
+      setup do 
+        @user = Factory(:user, :twitter => '@foo')
+      end
+
+      should 'save properly and remove the @ sign upon validation' do
+        @user.save!
+        assert_equal 'foo', @user.twitter
+      end
+
+    end
+
+  end
+
+  context 'calling main_url' do
+
+    context 'when the user does not have a twitter' do
+      setup do
+        @user = Factory(:user, :twitter => nil)
+      end
+
+      should 'return nil' do
+        assert_equal nil, @user.main_url
+      end
+    end
+
+    context 'when the user has a twitter' do
+      setup do
+        @user = Factory(:user, :twitter => 'foo')
+      end
+
+      should 'return the twitter url' do
+        assert_equal 'http://twitter.com/foo', @user.main_url
+      end
+    end
+
+  end
+
+  context 'calling has_link?' do
+
+    context 'when the user does not have a twitter' do
+      setup do
+        @user = Factory(:user, :twitter => nil)
+      end
+
+      should 'return false' do
+        assert_equal false, @user.has_link?
+      end
+    end
+
+    context 'when the user has a twitter' do
+      setup do
+        @user = Factory(:user, :twitter => 'foo')
+      end
+
+      should 'return the twitter url' do
+        assert_equal true, @user.has_link?
+      end
+    end
+
+  end
+
 end
