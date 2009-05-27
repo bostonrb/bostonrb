142337a0ce66330e6b2fbf812e03429f1937e300
Josh Nichols <josh@technicalpickles.com>
2009-05-26 23:56:54 -0400

    Added tweet formatting for [#37]

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -3,6 +3,13 @@ module TweetsHelper
     name = html_escape(tweet.user.twitter)
     text = html_escape(tweet.text)
     link = link_to(name, "http://twitter.com/#{name}")
-    "#{link} &ndash; #{auto_link(text)}"
+    "#{link} &ndash; #{tweet_format(text)}"
+  end
+
+  def tweet_format(text)
+    linked_text = auto_link(text)
+    linked_text.gsub(/@([\w\d_]+)/) do |match|
+      %Q{<a href="http://twitter.com/#{$1}">@#{$1}</a>}
+    end
   end
 end
--- test/unit/helpers/tweets_helper_test.rb
+++ test/unit/helpers/tweets_helper_test.rb
@@ -4,16 +4,37 @@ class TweetsHelperTest < ActionView::TestCase
   context "link_to_tweet" do
     setup do
       user   = Factory(:user, :twitter => "Croaky")
-      @tweet = Factory(:tweet, :user => user)
+      @tweet = Factory(:tweet, :user => user, :text => 'zomg guys')
+      stubs(:tweet_format).returns("FORMATTED TWEET")
+
       @link  = link_to_tweet(@tweet)
     end
 
-    should "be user's name and tweet" do
+    should "be user's name and formatted tweet" do
       link     = link_to(@tweet.user.twitter,
                          "http://twitter.com/#{@tweet.user.twitter}")
-      expected = "#{link} &ndash; #{@tweet.text}"
+      expected = "#{link} &ndash; FORMATTED TWEET"
       assert_equal expected, @link
     end
+
+    should "call tweet_format with tweet's text" do
+      assert_received(self, :tweet_format) {|expect| expect.with('zomg guys') }
+    end
+  end
+  
+  context "tweet_format" do
+    should "convert @ replies to links" do
+      text = "@techpickles lol, totally"
+      expected = %Q{<a href="http://twitter.com/techpickles">@techpickles</a> lol, totally}
+      assert_equal expected, tweet_format(text)
+    end
+
+    should "autolink URLs" do
+      text = "http://technicalpickles.com"
+      expected = %Q{<a href="http://technicalpickles.com">http://technicalpickles.com</a>}
+      assert_equal expected, tweet_format(text)
+    end
+
   end
 end
 
