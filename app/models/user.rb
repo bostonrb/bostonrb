17566020074170c4ba8e546d68f8f15a5a379d53
Dan Croak <dcroak@thoughtbot.com>
2009-05-27 00:12:27 -0400

    bringing sexy (most of user.rb) back

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,3 +9,40 @@ class User < ActiveRecord::Base
   has_many :projects
   has_one  :feed, :as => :feed_owner, :dependent => :destroy
   belongs_to :company
+
+  accepts_nested_attributes_for :feed, :reject_if => proc { |attrs| attrs.nil? || attrs.all? { |key, value| value.blank? } }
+
+  attr_accessible :twitter, :github, :feed_attributes, :company_id
+
+  validates_format_of :twitter, :with => /^\w{1,15}$/, :allow_nil => true
+  before_validation :cleanout_twitter
+
+  def to_s
+    twitter
+  end
+
+  # For now, the twitter url.
+  # Eventually we'll just use user_path for gravatar links
+  # and this method may just become main_external_url,
+  # which may be their blog or their twitter.
+  def main_url
+    return nil unless twitter
+    "http://twitter.com/#{twitter.downcase}"
+  end
+
+  def has_link?
+    !!main_url
+  end
+
+  private
+
+    def cleanout_twitter
+      if self.twitter
+        self.twitter.gsub!(/^@/, '')
+        if self.twitter =~ /^(https?:\/\/)?(www\.)?twitter\.com\/(\w+)$/
+          self.twitter = $3
+        end
+      end
+    end
+
+end
