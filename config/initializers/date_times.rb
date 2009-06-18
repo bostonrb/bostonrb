d46affc61eaa45489c8408746fe10004c9beba82
Josh Nichols <josh@technicalpickles.com>
2009-06-18 00:52:12 -0400

    Added times to events' display.

--- config/initializers/date_times.rb
+++ config/initializers/date_times.rb
@@ -1,6 +1,8 @@
 { :short_date => "%x",
   :long_date  => "%a, %b %d, %Y",
   :fancy_date => "%B %d, %Y",
+  :fancy_date_with_time => "%B %d, %Y %I:%M %p",
+  :time => '%I:%M %p',
   :european   => "%m.%d.%y" }.each do |k, v|
   ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(k => v)
 end
