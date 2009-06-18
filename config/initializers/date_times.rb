72d490445728cab43b67a7cd21939ac2d3081b53
Josh Nichols <josh@technicalpickles.com>
2009-06-18 01:06:57 -0400

    Added comments to show what date formats look like.

--- config/initializers/date_times.rb
+++ config/initializers/date_times.rb
@@ -1,8 +1,9 @@
-{ :short_date => "%x",
-  :long_date  => "%a, %b %d, %Y",
-  :fancy_date => "%B %d, %Y",
-  :fancy_date_with_time => "%B %d, %Y %I:%M %p",
-  :time => '%I:%M %p',
-  :european   => "%m.%d.%y" }.each do |k, v|
+{ :short_date => "%x",                          # 06/18/09
+  :long_date  => "%a, %b %d, %Y",               # Thu, Jun 18, 2009
+  :fancy_date => "%B %d, %Y",                   # June 18, 2009
+  :fancy_date_with_time => "%B %d, %Y %I:%M %p",# June 18, 2009 12:58 AM
+  :time => '%I:%M %p',                          # 12:58 AM 
+  :european   => "%m.%d.%y"                     # 06.18.09
+}.each do |k, v|
   ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.update(k => v)
 end
