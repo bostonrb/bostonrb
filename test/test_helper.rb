5f92ba2b0ccd6a4d0149d8ddc90465cd97c50bbb
Greg Sterndale <gsterndale@gmail.com>
2010-10-19 21:25:36 -0400

    Do not require ruby-debug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require 'ruby-debug'
 
 class ActiveSupport::TestCase
   include Mocha::API
