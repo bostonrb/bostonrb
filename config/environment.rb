4f43fe3973003064fbc982cb4b21a403ff27d2e2
Greg Sterndale <gsterndale@gmail.com>
2010-10-20 23:30:05 -0400

    Replace deprecated :session_key with :key

--- config/environment.rb
+++ config/environment.rb
@@ -13,8 +13,8 @@ Rails::Initializer.run do |config|
 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_boston_rb_session',
-    :secret      => 'a406a09e903c61fa55fadce763e5df7cae1320592290a64e0665412cd9aff9b18116daeab3309fe69bfb3ffc6da224671f5ea5ab96ad7245b276a06a448f2c0a'
+    :key    => '_boston_rb_session',
+    :secret => 'a406a09e903c61fa55fadce763e5df7cae1320592290a64e0665412cd9aff9b18116daeab3309fe69bfb3ffc6da224671f5ea5ab96ad7245b276a06a448f2c0a'
   }
   
   config.time_zone = 'Eastern Time (US & Canada)'
