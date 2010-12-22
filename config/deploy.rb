497426fa36104489e807fe652deff80180b86a0f
Greg Sterndale <gsterndale@gmail.com>
2010-12-21 21:31:54 -0500

    Use capistrano-gitflow for sane deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
 set :stages, %w(staging production)
 set :default_stage, 'staging'
 require 'capistrano/ext/multistage' rescue 'YOU NEED TO INSTALL THE capistrano-ext GEM'
+# needs to come after multistage
+require 'capistrano/gitflow' rescue 'YOU NEED TO INSTALL THE capistrano-gitflow GEM'
