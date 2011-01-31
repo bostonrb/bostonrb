593c5f8982bcfb60d11fc96237d3e91583e8be6f
Dan Croak <dcroak@thoughtbot.com>
2011-01-30 21:53:00 -0500

    bringing back flutie for its form styles

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,5 +1,5 @@
 class Mailer < ActionMailer::Base
-  default :to => "admin@bostonrb.org"
+  default :to => ["admin@bostonrb.org","dcroak@thoughtbot.com"]
 
   def proposal(sender, custom_body)
     @custom_body = custom_body
