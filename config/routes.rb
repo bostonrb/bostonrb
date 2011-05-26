5f6c08b92d8e476dc919c07f4b2c736b87c19649
Brian Cardarella <bcardarella@gmail.com>
2011-05-26 11:13:45 -0400

    More dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BostonRuby::Application.routes.draw do
+  %w{jobs projects users companies}.each do |dead_route|
+    match "/#{dead_route}" => redirect('/')
+  end
+
   root :to => 'high_voltage/pages#show', :id => 'home'
   get '/hackfests' => 'high_voltage/pages#show', :id => 'hackfests', :as => 'hackfests'
   get '/calendar' => 'high_voltage/pages#show', :id => 'calendar', :as => 'calendar'
-  match '/jobs' => redirect('/')
-  match '/projects' => redirect('/')
   resources :proposals, :only => [:new, :create]
   get '/presentations/month/:month/' => 'presentations#index', :as => 'month_presentations'
   resources :presentations, :only => [:index, :show]
