4047a571e371faf8fb671c6f23e555227e19647a
patricksrobertson <patricksrobertson@gmail.com>
2011-07-22 18:41:29 -0400

    Removing RSpec deprecation warning by only using one RSpec config block.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,4 +25,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  config.include EmailSpec::Helpers
+  config.include EmailSpec::Matchers  
 end
