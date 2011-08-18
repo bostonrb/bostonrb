3f94b15b5502ec06512023495675a1480d192474
patricksrobertson <patricksrobertson@gmail.com>
2011-08-18 08:24:12 -0400

    Moved full month & year time format to the time_formats initializer to be more consistent with formatting date/times.

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -1,7 +1,7 @@
 module PresentationsHelper
   def presentations_index_header
     if params[:month]
-      Date.parse(params[:month]).strftime('%B %Y')
+      Date.parse(params[:month]).to_time.to_s(:month_and_year)
     else
       'Past presentations'
     end
