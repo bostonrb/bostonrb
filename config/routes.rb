6d785eb1f23160bfdffa2ff01242ca069b802717
Brian Cardarella <bcardarella@gmail.com>
2012-05-31 10:13:18 -0400

    Removed Hackfests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ BostonRuby::Application.routes.draw do
   end
 
   root :to => 'high_voltage/pages#show', :id => 'home'
-  get '/hackfests' => 'high_voltage/pages#show', :id => 'hackfests', :as => 'hackfests'
   get '/calendar' => 'high_voltage/pages#show', :id => 'calendar', :as => 'calendar'
   resources :proposals, :only => [:new, :create]
   get '/presentations/month/:month/' => 'presentations#index', :as => 'month_presentations'
