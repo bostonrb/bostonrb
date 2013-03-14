7a37c8a85085a13917c95057d2a6bb0e23cfdced
Dan Pickett <dan.pickett@launchware.com>
2013-03-14 13:29:43 -0400

    remove specs for defunct feature

--- spec/support/blog.rb
+++ spec/support/blog.rb
@@ -1,7 +1,10 @@
 require 'fileutils'
 module Blog::TestHelpers
   def setup_blog_feeds(date)
-    FileUtils.remove_dir "#{Rails.root}/tmp/blog_feeds", true
-    FileUtils.ln_sf      "#{File.expand_path("../../blog_feeds/#{date}", __FILE__)}", "#{Rails.root}/tmp/blog_feeds"
+    feed_path = "#{Rails.root}/tmp/blog_feeds"
+    if FileTest.exists?(feed_path)
+      FileUtils.remove_dir feed_path, true
+      FileUtils.ln_sf      "#{File.expand_path("../../blog_feeds/#{date}", __FILE__)}", "#{Rails.root}/tmp/blog_feeds"
+    end
   end
 end
