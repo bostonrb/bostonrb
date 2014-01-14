31f5eabb007b586b21b791ebf6611d085f34133c
John Moon <johnmoonyy@gmail.com>
2014-01-14 09:58:09 -0500

    Add suite address and zip code to location
    Allow admin to input more detailed information

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,7 @@ class Location < ActiveRecord::Base
   validates_presence_of :street
   validates_presence_of :city
   validates_presence_of :state
+  validates_presence_of :zipcode
 
   has_many :events,
     inverse_of: :location
--- spec/features/admin_creates_new_location_spec.rb
+++ spec/features/admin_creates_new_location_spec.rb
@@ -5,8 +5,10 @@ feature 'Admin creates new location' do
     visit new_location_path
     fill_in 'Name', with: 'Brightcove Offices'
     fill_in 'Street', with: '290 Congress St'
+    fill_in 'Suite address', with: '3rd floor'
     fill_in 'City', with: 'Boston'
     fill_in 'State', with: 'MA'
+    fill_in 'Zipcode', with: '02125'
     click_on 'Create Location'
 
     expect(page).to have_content("Created location successfully")
