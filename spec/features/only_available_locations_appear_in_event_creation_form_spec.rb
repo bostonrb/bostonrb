27c10f1c1807ee3003c6dd4d34bf56041065ca2e
Justus Danny Eapen Jr <justuseapen@gmail.com>
2014-01-29 00:30:16 -0500

    refactored available locations spec

--- spec/features/only_available_locations_appear_in_event_creation_form_spec.rb
+++ spec/features/only_available_locations_appear_in_event_creation_form_spec.rb
@@ -7,9 +7,7 @@ feature 'Admin creates new event' do
   end
 
   scenario 'deleted location does not exist' do
-    location = Location.create(name: 'Mission Control', street: "33 Harrison", city: "Boston", state: "Massachusetts", zipcode: '02125', time_of_deletion: Time.now)
-    Location.create(name: 'false Control', street: "33 Harrison", city: "Boston", state: "Massachusetts", zipcode: '02125')
-
+    location = FactoryGirl.create(:location, time_of_deletion: Time.now )
     visit new_admin_event_path
     find_field('Location').should_not have_content('Mission Control')
   end
