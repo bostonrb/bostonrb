8f07372cb80549355dd11714040fe1908d5e1a9f
Patrick Robertson <patricksrobertson@gmail.com>
2016-06-03 11:26:08 -0400

    Remove twitter config.

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,2 @@
 if Rails.env.production?
-  Twitter.configure do |config|
-    config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-    config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
-    config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
-  end
 end
