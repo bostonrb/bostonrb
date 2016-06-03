36a70b81c40db4ad5c95bb56e2b57c970e0ef6d9
Patrick Robertson <patricksrobertson@gmail.com>
2016-06-03 11:07:40 -0400

    Fix basic auth shizzz.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  #http_basic_authenticate_with name: BostonRuby::Admin[:username], password: BostonRuby::Admin[:password]
+  http_basic_authenticate_with name: BostonRuby::Admin[:username], password: BostonRuby::Admin[:password]
   # Put the before filter to authenticate here I think
 
   before_filter :check_for_authenticated_user
